6cc0fb9520257011e4807e0869a058c79b8d739e
xach <xach@xach-CX61-2PC>
2016-04-23 07:11:09 -0400

    init

---
+++ main.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+  console.log(req);
+  console.log(res);
+});
+
+app.listen(3247, function () {
+  console.log('Example app listening on port 3247!');
+});
